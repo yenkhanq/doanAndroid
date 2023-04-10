68c425a8b9bb95fc79f4f6ec2372651c4f8e95bc
yenkhanq <yenk946@gmail.com>
2023-04-10 21:20:08 +0700

    app bt lon

--- app/src/main/java/com/example/appbtlon/Mainsinhvien.java
+++ app/src/main/java/com/example/appbtlon/Mainsinhvien.java
@@ -10,5 +10,7 @@ public class Mainsinhvien extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainsinhvien);
+
+        //ghi chu main
     }
 }
